4408b7b6773b20dd947fba08fe422b5f66d38973
(s) Simon Harrington <sharrington1@pm7-0089.uopnet.plymouth.ac.uk>
2017-11-22 13:14:52 +0000

    initial commit

---
+++ Pixal.js
@@ -0,0 +1,133 @@
+//https://github.com/stavrosdidakis/DAT-GAD-405_2017
+//https://p5js.org
+
+//DAT405 / GAD405
+//02_Coordinates
+
+//Initialization function
+function setup() {
+  //Set the size of rendering window - pixels
+console.log("Initialization: OK")
+  createCanvas(400, 400);
+
+}
+//Rendering function
+function draw() {
+
+  background(192,192,192);
+//boo bottom line, Start right
+fill(0);
+rectMode(CENTER);
+rect(180,300,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(190,300,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(200,300,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(210,300,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(220,300,10,10)
+
+//boo bottom shift up one direction left
+fill(0);
+rectMode(CENTER);
+rect(170,290,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(160,290,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(150,290,10,10)
+
+//shift up and left
+fill(0);
+rectMode(CENTER);
+rect(140,280,10,10)
+
+//shift up and left
+
+fill(0);
+rectMode(CENTER);
+rect(130,270,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(130,260,10,10)
+
+//shift up and left, left hand side
+fill(0);
+rectMode(CENTER);
+rect(120,250,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(120,240,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(120,230,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(120,220,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(120,210,10,10)
+
+//shift up and right
+fill(0);
+rectMode(CENTER);
+rect(130,200,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(130,190,10,10)
+
+//shift up and right
+fill(0);
+rectMode(CENTER);
+rect(140,180,10,10)
+
+//shift up and right
+fill(0);
+rectMode(CENTER);
+rect(150,170,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(160,170,10,10)
+
+// top line
+fill(0);
+rectMode(CENTER);
+rect(170,160,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(180,160,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(190,160,10,10)
+
+fill(0);
+rectMode(CENTER);
+rect(200,160,10,10)
+
+
+
+
+console.log("Rendering...")
+noLoop();
+}
